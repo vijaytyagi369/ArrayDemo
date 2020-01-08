e0963323c1490b9a4b08850df67118f13bb73040
dbc2201 <divyanshb22@gmail.com>
2020-01-08 16:36:31 +0530

    add 2 methods

--- src/array/ArrayDemo.java
+++ src/array/ArrayDemo.java
@@ -7,6 +7,37 @@
 
 package array;
 
+import java.util.Arrays;
+
 public class ArrayDemo {
 
+    public static void main(String[] args) {
+/*
+        for (int i = 0; i < integers.length; i++) {
+            System.out.print(integers[i] + ", ");
+        }
+*/
+        /*for (int i : integers) {
+            System.out.print(i + ", ");
+        }*/
+        int[] integers = new int[10];
+        printArrayValues(integers);
+        initialiseIntegerArray(integers);
+        printArrayValues(integers);
+
+    }
+
+    /*public static boolean searchInArray(int[] array, int value) {
+
+    }*/
+
+    public static void printArrayValues(int[] integers) {
+        System.out.println(Arrays.toString(integers));
+    }
+
+    public static void initialiseIntegerArray(int[] integerArray) {
+        for (int i = 0; i < integerArray.length; i++) {
+            integerArray[i] = (i + 1);
+        }
+    }
 }

33718ef14e253d6dfa2df6df88918d1e0ada9f84
dbc2201 <divyanshb22@gmail.com>
2020-01-09 10:08:42 +0530

    add overlaoded method for inserting values at an index

--- src/array/ArrayFunctions.java
+++ src/array/ArrayFunctions.java
@@ -18,4 +18,17 @@ public class ArrayFunctions {
     public void insertValue(int[] array, int value) {
 
     }
+
+    /**
+     * This method is the overloaded form of the {@code insertValue()} method which
+     * lets the user tell the index at which the value is to be inserted.
+     * If the index already has a value, this method will override it.
+     *
+     * @param array the integer array in which the value is to be inserted
+     * @param value the value that is to be inserted
+     * @param index the index at which the value is to be inserted
+     */
+    public void insertValue(int[] array, int value, int index) {
+
+    }
 }

1486ed3c33f438b2f5c96f36d6f5e0234fee291a
dbc2201 <divyanshb22@gmail.com>
2020-01-09 10:06:47 +0530

    add JavaDoc comment for insertValue() method

--- src/array/ArrayFunctions.java
+++ src/array/ArrayFunctions.java
@@ -8,6 +8,13 @@
 package array;
 
 public class ArrayFunctions {
+    /**
+     * This method will insert an integer value in an integer array at the last index.
+     * If the array is already full, it will do nothing.
+     *
+     * @param array the integer array in which the value is to be inserted
+     * @param value the value that is to be inserted
+     */
     public void insertValue(int[] array, int value) {
 
     }
